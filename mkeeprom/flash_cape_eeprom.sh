3e424757acc6e2196d18de6bc5b57ed8788a2c36
Bartosz Golaszewski <bgolaszewski@baylibre.com>
2015-09-09 12:54:58 +0200

    mkeeprom: automate flashing of the cape eeprom
    Also: use at24c02 eeprom which we are able to read as opposed to at24c64.
    
    Signed-off-by: Bartosz Golaszewski <bgolaszewski@baylibre.com>

--- mkeeprom/flash_cape_eeprom.sh
+++ mkeeprom/flash_cape_eeprom.sh
@@ -53,29 +53,45 @@ $SERIAL
 \n" > $INPUT_PATH
 }
 
-if [ ! "$#" == "2" ]
+do_i2cget() {
+	i2cget -y 1 0x5c $1 2> /dev/null || echo 00
+}
+
+cape_read_serial() {
+	printf "%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n" \
+		$(do_i2cget 0x80) $(do_i2cget 0x81) $(do_i2cget 0x82) $(do_i2cget 0x83) \
+		$(do_i2cget 0x84) $(do_i2cget 0x85) $(do_i2cget 0x86) $(do_i2cget 0x87) \
+		$(do_i2cget 0x88) $(do_i2cget 0x89) $(do_i2cget 0x8a) $(do_i2cget 0x8b) \
+		$(do_i2cget 0x8c) $(do_i2cget 0x8d) $(do_i2cget 0x8e) $(do_i2cget 0x8f)
+}
+
+if [ ! "$#" == "1" ]
 then
 	echo "usage:"
-	echo "	flash_cape_eeprom.sh <path to mkeeprom> <serial number>"
+	echo "	flash_cape_eeprom.sh <path to mkeeprom>"
+	echo
+	echo "EEPROM address is expected to be 0x54"
 	exit 1
 fi
 
 MKEEPROM=$1
-SERIAL=$2
+OUT=./data.eeprom
 
-SERIAL_SIZE=$(echo $SERIAL | wc -c)
-if [ $SERIAL_SIZE -gt 17 ] # Account for newline
-then
-	echo "serial number must be at most 16 chars in length"
-	exit 1
-fi
+SERIAL=$(cape_read_serial)
+
+modprobe at24 2> /dev/null
+modprobe gpio-pca953x 2> /dev/null
 
 set -e
+echo 24c02 0x54 > /sys/class/i2c-adapter/i2c-1/new_device
+echo pca9534 0x21  > /sys/class/i2c-adapter/i2c-1/new_device
+echo 505 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio505/direction
 make_input
 cat $INPUT_PATH | $MKEEPROM > /dev/null
+cat $OUT > /sys/class/i2c-dev/i2c-1/device/1-0054/eeprom
+echo 505 > /sys/class/gpio/unexport
+echo 0x54 > /sys/class/i2c-adapter/i2c-1/delete_device
+echo 0x21  > /sys/class/i2c-adapter/i2c-1/delete_device
 
-echo "EEPROM data file stored at data.eeprom"
-echo "This data file can be put in the BeagleBone EEPROM via the following command on a BeagleBone:"
-echo "	'cat data.eeprom >/sys/bus/i2c/drivers/at24/1-005x/eeprom'"
-echo
-echo "Where:  5x is 54, 55, 56, 57 depending on Cape addressing."
+echo "data written to eeprom"
